2445ece7197f7ddfb314bcd7fb136ee361c53d43
LONG <1558193230@qq.com>
2019-03-08 14:40:05 +0800

    修改摄像头的复位引脚；修改文件系统的使用空间：改为后16M 以及更新spi flash的驱动

--- 32. uCOSIII_emWin528_Demo（即开机例程）/USER/Bsp/ov5640/bsp_ov5640.h
+++ 32. uCOSIII_emWin528_Demo（即开机例程）/USER/Bsp/ov5640/bsp_ov5640.h
@@ -110,10 +110,9 @@ typedef enum
 #define DCMI_PWDN_GPIO_CLK         	RCC_AHB1Periph_GPIOG
 #define DCMI_PWDN_GPIO_PIN         	GPIO_Pin_3
 //RST
-#define DCMI_RST_GPIO_PORT        	GPIOG
-#define DCMI_RST_GPIO_CLK         	RCC_AHB1Periph_GPIOG
-#define DCMI_RST_GPIO_PIN         	GPIO_Pin_2
-
+#define DCMI_RST_GPIO_PORT        	GPIOB
+#define DCMI_RST_GPIO_CLK         	RCC_AHB1Periph_GPIOB
+#define DCMI_RST_GPIO_PIN         	GPIO_Pin_5
 
 //数据信号线
 #define DCMI_D0_GPIO_PORT        	GPIOH

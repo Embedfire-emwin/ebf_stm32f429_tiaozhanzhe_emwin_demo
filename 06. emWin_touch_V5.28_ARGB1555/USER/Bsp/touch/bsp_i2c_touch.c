69f5e18cb36e7dc37b9f4f5e1d760266387018c0
LONG <1558193230@qq.com>
2019-03-08 15:22:20 +0800

    更行SDRAM驱动和触摸板的复位引脚

--- 06. emWin_touch_V5.28_ARGB1555/USER/Bsp/touch/bsp_i2c_touch.c
+++ 06. emWin_touch_V5.28_ARGB1555/USER/Bsp/touch/bsp_i2c_touch.c
@@ -85,7 +85,7 @@ static void I2C_GPIO_Config(void)
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_UP;
+  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_DOWN;
   GPIO_Init(GTP_RST_GPIO_PORT, &GPIO_InitStructure);
   
   /*!< Configure INT */   
